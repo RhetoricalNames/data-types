5639adae7ab7dc0b125b7b1f854faa4459d96928
Vincent <vincentz92@nycstudents.net>
2025-09-11 10:32:13 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+    print(values[0])
+print(values[6]) """
+
+""" x = "this is a thing"
+y= x.split( )
+#split ( ) splits the words between spacebars
+z = y[4]
+print(y)
+print(z) """
+
+def message(input):
+    print(input)
+
+message("Input a sentence")
+
+x = "input()"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) 
