40410ed0145be6633201fc3dfa9e07e5a76df914
Vincent <vincentz92@nycstudents.net>
2025-09-12 10:31:56 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,21 +4,42 @@ for i in values:
     print(i)
     print(values[0])
 print(values[6]) """
+def message(input):
+    print(input)
 
 """ x = "this is a thing"
 y= x.split( )
 #split ( ) splits the words between spacebars
-z = y[4]
+z = y[3]
 print(y)
-print(z) """
+print(z)
 
-def message(input):
-    print(input)
 
-message("Input a sentence")
 
-x = "input()"
-y= x.split( )
-z = y[0]
-print(y)
-print(z) 
+word_counter = input("Input a sentence")
+print(len(word_counter.split( ))) """
+""" 
+x = "testa"
+print(f"hello {x}") """
+
+
+""" temp = 68
+if temp > 68:
+    print('warm')
+elif temp == 68: #'elif' is 'else if'
+    print('perfect')
+else:
+    print('cold') """
+
+number_finder = input("Input a number")
+if number_finder % 2 == 0:
+    print(even)
+    else
+
+
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
