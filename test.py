05a98e81f31ef382c1f0fa6affeff5a64a6f034c
Vincent <vincentz92@nycstudents.net>
2025-09-16 10:16:49 -0400

    Update test.py

--- test.py
+++ test.py
@@ -37,15 +37,40 @@ if int(number_finder) % 2 == 0:
 else:
     print("odd") """
 
-bill = input("What's the bill?")
+""" bill = input("What's the bill?")
+
 service = input("How was the service? Bad, okay, good, or great?")
 if service == "bad":
-    print(int(bill))
+    print((bill))
 elif service == "okay":
-    print()
+    print(float(bill) * 1.15)
+elif service == "good":
+    print(float(bill) * 1.20)
+elif service == "great":
+    print(float(bill) * 1.25) """
+
 
 """ day_of_week = input("what day is it? ")
 if day_of_week == "Friday":
     print("correct")
 else:
     print("incorrect") """
+""" x = 1
+if x == 2:
+    print("x equals 2")
+elif x == 3:
+    print("x equals 3")
+elif x == 4:
+    print("x is 4")
+elif x == 1:
+    print("x = 1") """
+
+number = input("Input a number")
+number = int(number)
+for i in range(factor):
+    factor = 2
+    if (number % factor) == 0:
+        factorlist = [factor]
+    factor = factor + 1
+onelist = [1]
+print(onelist + factorlist)
